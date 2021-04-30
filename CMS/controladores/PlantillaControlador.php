e303d8662b9d5a7a2641b9042b312921da6d3f05
Yeimer Piendo <yjpinedo@unicesar.edu.co>
2021-04-29 21:22:59 -0500

    Integrando plantilla html a CMS

---
+++ CMS/controladores/PlantillaControlador.php
@@ -0,0 +1,7 @@
+<?php
+
+class PlantillaControlador {
+    public function traerPlantilla () {
+        include 'vistas/plantilla.php';
+    }
+}
---
+++ CMS/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'controladores/PlantillaControlador.php';
+$plantilla = new PlantillaControlador();
+$plantilla->traerPlantilla();
